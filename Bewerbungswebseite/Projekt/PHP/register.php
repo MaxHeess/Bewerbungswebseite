356928a7dbff1ddd422284f8d4f1064afffd0f8b
Maximilian Heeß <max-heess@gmx.de>
2025-05-05 13:44:34 +0200

    Update register.php

--- Bewerbungswebseite/Projekt/PHP/register.php
+++ Bewerbungswebseite/Projekt/PHP/register.php
@@ -48,7 +48,7 @@ include('../PHP/header.php');
 
         $servername = "localhost";
         $username = "d04212b7";
-        $password = "Artus.2008";
+        $password = "";
         $dbname = "d04212b7";
         
         try {
